db2a4d6249a821594c13c548cd7e553af9e94d59
Almog Wertzberger <Almogish@gmail.com>
2021-09-16 18:16:22 +0300

    adding functionality

--- src/Article.js
+++ src/Article.js
@@ -1,7 +1,19 @@
-import React from 'react'
-import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
-}
+import React from "react";
+import moment from "moment";
+const Article = (article) => {
+  const { id, title, length, date, snippet } = article;
 
-export default Article
+  return (
+    <article className="post">
+      <h2>{title}</h2>
+      <div className="post-info">
+        <span>{moment(date).format("MMM Do dddd, YYYY ")}</span>
+
+        <span>{length} min read</span>
+      </div>
+      <p>{snippet}</p>
+    </article>
+  );
+};
+
+export default Article;
